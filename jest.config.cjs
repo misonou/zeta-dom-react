c4bedef162741f8b7dd476d598a33d850d0690f3
Takeru <takeru.moe@gmail.com>
2024-05-26 22:32:54 +0800

    chore: add support for react 19

--- jest.config.cjs
+++ jest.config.cjs
@@ -28,8 +28,10 @@ if (process.env.CI !== 'true' && require('fs').existsSync('../zeta-dom')) {
         "^zeta-dom/(.*)$": "<rootDir>/../zeta-dom/src/$1"
     };
 }
-if (process.env.REACT_VERSION) {
-    const runtimeDir = `<rootDir>/tests/runtime/react${process.env.REACT_VERSION}`;
+
+const REACT_VERSION = parseInt(process.env.REACT_VERSION);
+if (REACT_VERSION) {
+    const runtimeDir = `<rootDir>/tests/runtime/react${REACT_VERSION}`;
     config.cacheDirectory = `${runtimeDir}/.cache`;
     config.moduleNameMapper = {
         ...config.moduleNameMapper,
@@ -41,7 +43,7 @@ if (process.env.REACT_VERSION) {
         "node_modules"
     ];
 }
-if (process.env.REACT_VERSION === '18') {
+if (REACT_VERSION >= 18) {
     config.moduleNameMapper = {
         ...config.moduleNameMapper,
         "@testing-library/react-hooks": "@misonou/test-utils/polyfill/react18/renderHook"
