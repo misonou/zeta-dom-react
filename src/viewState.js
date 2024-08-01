d9c6fa56e24b3d88a23dda2b918bee6e15d97e59
Takeru <takeru.moe@gmail.com>
2024-08-01 12:21:48 +0800

    fix(viewState): noop storage cause error in dev strict mode

--- src/viewState.js
+++ src/viewState.js
@@ -4,15 +4,16 @@ import { useSingleton } from "./hooks.js";
 
 /** @type {React.Context<import("./viewState").ViewStateProvider | null>} */
 const ViewStateProviderContext = createContext(null);
-const noopStorage = Object.freeze({
-    get: noop,
-    set: noop
-});
 
 export const ViewStateProvider = ViewStateProviderContext.Provider;
 
 export function useViewState(key) {
     var uniqueId = useState(randomId)[0];
     var provider = useContext(ViewStateProviderContext);
-    return useSingleton((provider && key && provider.getState(uniqueId, key)) || noopStorage);
+    return useSingleton(function () {
+        return (provider && key && provider.getState(uniqueId, key)) || {
+            get: noop,
+            set: noop
+        };
+    }, [provider, key, uniqueId]);
 }
